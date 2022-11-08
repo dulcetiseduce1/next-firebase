c4a813c811e21aeaa3b45013e80e6c46bd153f9b
dulcetiseduce1 <mariusdulce@tuta.io>
2022-11-08 19:50:36 +0100

    basic UI

---
+++ pages/admin/index.tsx
@@ -0,0 +1,7 @@
+export default function AdminPostPage({}) {
+    return (
+        <main>
+            <h1>Admin Posts</h1>
+        </main>
+    )
+}
---
+++ pages/enter.tsx
@@ -0,0 +1,11 @@
+import Loader from '../components/Loader'
+
+
+export default function EnterPage({}) {
+    return (
+        <main>
+            <Loader show/>
+            <h1>Sign Up</h1>
+        </main>
+    )
+}
