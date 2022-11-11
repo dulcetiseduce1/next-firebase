dab033ef63e0fd3f8445834479d5a479bc4fff64
dulcetiseduce1 <mariusdulce@tuta.io>
2022-11-11 04:51:36 +0100

    metatags

--- components/Metatags.tsx
+++ components/Metatags.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 
 export default function Metatags({
-    title = 'The Full Next.js + Firebase Course',
-    description = 'A complete Next.js + Firebase course by Fireship.io',
-    image = 'https://fireship.io/courses/react-next-firebase/img/featured.png',
+    title = 'Podas',
+    description = 'Social Test',
+    image = 'https://imgur.com/a/VO9yTMk',
 }): JSX.Element {
     return (
         <Head>
